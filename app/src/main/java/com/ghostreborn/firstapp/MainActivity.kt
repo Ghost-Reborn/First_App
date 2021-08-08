953225971b108ea3ee409f6203003e8d5c2d6a95
Ghost-Reborn <abhinandammanamkandy@gmail.com>
2021-08-08 20:39:35 +0530

    Intent * Finished First App project

--- app/src/main/java/com/ghostreborn/firstapp/MainActivity.kt
+++ app/src/main/java/com/ghostreborn/firstapp/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.ghostreborn.firstapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    /* Called when user taps Send Button */
+    fun sendMessage(view: View) {
+        val editText = findViewById<EditText>(R.id.editTextTextPersonName)
+        val message = editText.text.toString()
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(Companion.EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
+    }
+
+    companion object {
+        const val EXTRA_MESSAGE = "com.ghostreborn.firstapp.EXTRA_MESSAGE"
+    }
+
 }
